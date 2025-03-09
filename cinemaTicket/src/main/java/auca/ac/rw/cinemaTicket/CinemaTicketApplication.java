cd9637b62f0e71b62dd68a4bb08aab82bd3b117f
kezagiselle <kezagiselle776@gmail.com>
2025-03-09 09:55:44 +0200

    java project

---
+++ cinemaTicket/src/main/java/auca/ac/rw/cinemaTicket/CinemaTicketApplication.java
@@ -0,0 +1,13 @@
+package auca.ac.rw.cinemaTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketApplication.class, args);
+	}
+
+}
---
+++ cinemaTicket/src/test/java/auca/ac/rw/cinemaTicket/CinemaTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package auca.ac.rw.cinemaTicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
