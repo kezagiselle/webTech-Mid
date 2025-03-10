c1b933d6e22721ca526c94cfa0e42e3e90b63550
kezagiselle <kezagiselle776@gmail.com>
2025-03-10 06:41:52 +0200

    something else added

--- cinemaTicket/src/main/java/auca/ac/rw/cinemaTicket/controllers/CategoryUnit.java
+++ cinemaTicket/src/main/java/auca/ac/rw/cinemaTicket/controllers/CategoryUnit.java
@@ -1,30 +1,10 @@
 package auca.ac.rw.cinemaTicket.controllers;
 
-import auca.ac.rw.cinemaTicket.models.ECategoryUnit;
-import auca.ac.rw.cinemaTicket.repositories.CategoryUnitRepository;
+
 
 public class CategoryUnit {
 
-    public void setName(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setName'");
-    }
-
-    public void setCategoryType(ECategoryUnit categoryType) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setCategoryType'");
-    }
-
-    public void setDescription(String description) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setDescription'");
-    }
-
-    public void setParentCategory(CategoryUnit parentCategory) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setParentCategory'");
-    }
-
+}
  
 
 
@@ -36,4 +16,4 @@ public class CategoryUnit {
 
 
 
-}
+
