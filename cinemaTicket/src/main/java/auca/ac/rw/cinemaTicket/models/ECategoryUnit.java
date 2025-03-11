2443d1da49c79758af2e9d72307d42e18a243575
kezagiselle <kezagiselle776@gmail.com>
2025-03-11 14:02:42 +0200

    testing some codes

--- cinemaTicket/src/main/java/auca/ac/rw/cinemaTicket/models/ECategoryUnit.java
+++ cinemaTicket/src/main/java/auca/ac/rw/cinemaTicket/models/ECategoryUnit.java
@@ -3,3 +3,18 @@ package auca.ac.rw.cinemaTicket.models;
 public enum ECategoryUnit {
     ACTION, ADVENTURE, COMEDY, DRAMA, HORROR, THRILLER, FRICTION, FANTASY, ROMANCE, MYSTERY
 }
+
+// By Audience
+// Kids
+// Teen
+// Family
+// Adult
+// By Production Type
+// Animated
+// Live-Action
+// Documentary
+// Short Film
+// By Release Type
+// Theatrical
+// Direct-to-Streaming
+// TV Movie
