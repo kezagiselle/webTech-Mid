c076bb10e3622d8f69202250d10e142630983393
kezagiselle <kezagiselle776@gmail.com>
2025-05-21 13:13:17 +0200

    changes

--- cinemaTicket/src/main/java/auca/ac/rw/cinemaTicket/services/UserDetailsServiceImpl.java
+++ cinemaTicket/src/main/java/auca/ac/rw/cinemaTicket/services/UserDetailsServiceImpl.java
@@ -3,12 +3,14 @@ package auca.ac.rw.cinemaTicket.services;
 import auca.ac.rw.cinemaTicket.models.UserModel;
 import auca.ac.rw.cinemaTicket.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
